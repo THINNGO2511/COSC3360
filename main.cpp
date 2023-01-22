9c6f5af339ef3228a89e06c0c550eeb2a2815245
Thinh Ngo <ngothinh2511@gmail.com>
2023-01-21 21:30:48 -0600

    Initialize files

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "huffmanTree.h"
+using namespace std;
+
+int main(int argc, char** argv){
+    return 0;
+}
+
