2db9273a184dc917bfc582f3aa6cb2eb6dd589de
Thinh Ngo <ngothinh2511@gmail.com>
2023-01-24 00:32:37 -0600

    finished pq

---
+++ huffmanTree.cpp
@@ -0,0 +1,6 @@
+#include "huffmanTree.h"
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
---
+++ huffmanTree.h
@@ -0,0 +1,14 @@
+#ifndef HUFFMANTREE_H
+#define HUFFMANTREE_H
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+#define MAX_TREE_HT 100
+
+class Tree{
+
+};
+
+#endif
